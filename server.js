98785cdd99eb4e53d779bc76cc0a9750e1f22d4c
seamanhd <seamanhd@gmail.com>
2017-03-16 21:45:29 -0400

    tried debugging - changed a few things

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public/test.html"));
+app.use(express.static(process.cwd() + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -23,7 +23,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_Controller.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
